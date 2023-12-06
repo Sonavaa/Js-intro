649b9b32574edd610afaa660df22c8cd7a54902b
Sonava <sonava@code.edu.az>
2023-12-06 23:58:25 +0400

    All Done

---
+++ script.js
@@ -0,0 +1,25 @@
+//#region BMI Carculator
+// let bodyWeight = prompt("Enter your body weight")
+// let height = prompt("Enter your height")
+// if (height>100){
+//     height=height/100
+// }
+//     let BMI = bodyWeight / height**2
+// console.log(BMI)
+//#endregion
+
+//#region Password Length Carculator
+// let password = prompt("Enter your password.")
+// if(password.length>=8){
+//     console.log("Strong Password")
+// }
+// else{
+// console.log(`Weak Password. You have to add at least ${8-password.length} more characters.`)
+// }
+//#endregion
+
+//#region Foot to meter
+// let foot = prompt("Enter foot value")
+// let meter = foot * 0.3048
+// console.log(meter)
+//#endregion
